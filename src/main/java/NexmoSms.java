942ae266d7da4e85a6702a9ff5bb4449dafcc9a9
StayHungryStayFoolish <bonismo@hotmail.com>
2017-12-29 15:33:06 +0800

    NexmoDemo Commit

--- src/main/java/NexmoSms.java
+++ src/main/java/NexmoSms.java
@@ -8,20 +8,25 @@ import com.nexmo.client.sms.messages.TextMessage;
 import java.io.IOException;
 
 public class NexmoSms {
+
     private final static String API_KEY = "90f3214c";
 
     private final static String API_SECRET = "3b7a016c72e5163f";
 
-    private final static String FROM_NUMBER = "+8617010016050";
-    private final static String TO_NUMBER = "+8613020005357";
+    private final static String FROM_NUMBER = "8617610016050";
+    private final static String TO_NUMBER = "8613020005357";
 
     public static void main(String[] args) throws IOException, NexmoClientException {
         AuthMethod auth = new TokenAuthMethod(API_KEY, API_SECRET);
         NexmoClient client = new NexmoClient(auth);
-        SmsSubmissionResult[] results = client.getSmsClient().submitMessage(new TextMessage(TO_NUMBER, FROM_NUMBER, "Hello"));
+        TextMessage message = new TextMessage("NEXMO", FROM_NUMBER, "你好,this is a test !");
+
+        SmsSubmissionResult[] results = client.getSmsClient().submitMessage(message);
 
         for (SmsSubmissionResult result : results) {
             System.out.println("=====>"+result);
         }
+
+
     }
 }
